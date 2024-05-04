2f81f1be6c56764d5f7c1e5c873f69a977ed18ca
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-04 11:06:14 +0200

    Fetching data from local database server

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,12 @@ import React from 'react';
 import classes from './page.module.css'
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from "@/lib/meals";
 
-function MealsPage() {
+async function MealsPage() {
+
+    const meals = await getMeals();
+
     return (
         <>
             <header className={classes.header}>
@@ -20,7 +24,7 @@ function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     );
